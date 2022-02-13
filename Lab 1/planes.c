0e74ed544a9bfc3182c053b6fce55fb10de60378
Michal Tesnar <michal.tesnar007@gmail.com>
2022-02-13 21:38:57 +0100

    Playing around

--- Lab 1/planes.c
+++ Lab 1/planes.c
@@ -7,91 +7,84 @@
  * author: Michal Tešnar (m.tesnar@student.rug.nl, s4740556)
  * author: Wojciech Trejter (w.trejter@student.rug.nl, s4296850)
  *
- * date 09.02.2022
+ * date 10.02.2022
  *
  * description: This program models an airport with a waiting runway
  * and a hangar for reparation. It takes input of planes landing and
  * whether they need to be repaired and prints the order in which
  * they leave.
  */
- 
+
 #include "LibStack.h"
 #include "LibQueue.h"
 
-int main(int argc, char *argv[]) {
-	// get the amount of landing planes
-	int planes;
-	scanf("%i", &planes);
-	
-	// define runway (queue) and hangar (stack) and counters to track amount of planes on them
-	Queue runway = newQueue(8);
-	int runwayCounter = 0;
-	Stack hangar = newStack(6);
-	int hangarCounter = 0;
-	
-	// define variables to hold input 
-	int planeNumber;
-	char repair[3];
-  
-	// procedure while planes are landing
-	for(int i = 0; i < planes; i++){
-		
-		// if runway is full then all planes leave on it leave the airport
-		if(runwayCounter == 7){
-			while(runwayCounter != 0){
-				printf("%i\n", dequeue(&runway));
-				runwayCounter--;
-			}
-		}
-		
-		// if hangar is full then all planes leave the runway, planes from hangar go to the runway	
-		if(hangarCounter == 5){
-			while(runwayCounter != 0){
-				printf("%i\n", dequeue(&runway));
-				runwayCounter--;
-			}
-			while(hangarCounter != 0){
-				enqueue(pop(&hangar), &runway);
-				hangarCounter--;
-				runwayCounter++;
-			}
-		}
-		
-		// now neither hangar nor runway are full, we can add planes to them
-		// get input line: identifier or the plain and whether it needs to be repaired
-		scanf("%i %s", &planeNumber, repair);
-		
-		// if planes need to be repaired then it goes to the hangar
-		if(repair[0] == 'y'){
-			push(planeNumber, &hangar);
-			hangarCounter++;
-		// if that is not the case, we put them into the waiting runway
-		} else {
-			enqueue(planeNumber, &runway);
-			runwayCounter++;
-		}
-	}
-	
-	// now all planes have landed
-	// first we empty the runway
-	while(runwayCounter != 0){
-		printf("%i\n", dequeue(&runway));
-		runwayCounter--;
-	}
-	// then hangar empties onto runway
-	while(hangarCounter != 0){
-		enqueue(pop(&hangar), &runway);
-		hangarCounter--;
-		runwayCounter++;
-	}
-	// then we again empty the runway
-	while(runwayCounter != 0){
-		printf("%i\n", dequeue(&runway));
-		runwayCounter--;
-	}
+void emptyRunway(int *runwayCounter, Queue *runway) {
+    while (*runwayCounter != 0) {
+        printf("%i\n", dequeue(runway));
+        *runwayCounter -= 1;
+    }
+}
 
-	// free the memory of stacked hangar and runway
-	freeStack(hangar);
-	freeQueue(runway);
-	return 0;
+void emptyHangar(int *runwayCounter, Queue *runway, int *hangarCounter, Stack *hangar) {
+    emptyRunway(runwayCounter, runway);
+    while (*hangarCounter != 0) {
+        enqueue(pop(hangar), runway);
+        *hangarCounter -= 1;
+        *runwayCounter += 1;
+    }
+}
+
+int main(int argc, char *argv[]) {
+    // get the amount of landing planes
+    int planes;
+    scanf("%i", &planes);
+
+    // define runway (queue) and hangar (stack) and counters to track amount of planes on them
+    Queue runway = newQueue(8);
+    int runwayCounter = 0;
+    Stack hangar = newStack(6);
+    int hangarCounter = 0;
+
+    // define variables to hold input
+    int planeNumber;
+    char repair[4];
+
+    // procedure while planes are landing
+    for (int i = 0; i < planes; i++) {
+
+        // if runway is full then all planes leave on it leave the airport
+        if (runwayCounter == 7) {
+            emptyRunway(&runwayCounter, &runway);
+        }
+
+        // if hangar is full then all planes leave the runway, planes from hangar go to the runway
+        if (hangarCounter == 5) {
+            emptyHangar(&runwayCounter, &runway, &hangarCounter, &hangar);
+        }
+
+        // now neither hangar nor runway are full, we can add planes to them
+        // get input line: identifier or the plain and whether it needs to be repaired
+        scanf("%i %s", &planeNumber, repair);
+
+        // if planes need to be repaired then it goes to the hangar
+        if (repair[0] == 'y') {
+            push(planeNumber, &hangar);
+            hangarCounter++;
+            // if that is not the case, we put them into the waiting runway
+        } else {
+            enqueue(planeNumber, &runway);
+            runwayCounter++;
+        }
+    }
+
+    // now all planes have landed
+    // first we empty the hangar which also empties the runway
+    emptyHangar(&runwayCounter, &runway, &hangarCounter, &hangar);
+    // then we empty the runway
+    emptyRunway(&runwayCounter, &runway);
+
+    // free the memory of stacked hangar and runway
+    freeStack(hangar);
+    freeQueue(runway);
+    return 0;
 }
